7a8b43eeb22619b7f25ea6332edcee0b9168e52b
Donna Lee <donna.lee@xtremelabs.com>
2014-01-06 16:17:14 -0500

    Update from demo to sample application

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 ## README
 
-# Ruby on Rails Tutorial: demo app
+# Ruby on Rails Tutorial: sample application
 
-This is the demo application for the 
-[*Ruby on Rails Tutorial*](http://railstutorial.org/)
-by [Michael Hartl](http://michaelhartl.com).
+This is the sample application for
+the [*Ruby on Rails Tutorial*](http://railstutorial.org/)
+by [Michael Hartl](http://michaelhartl.com/).
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,22 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-DemoApp::Application.config.secret_key_base = '61cbad0e755c925c75461c704ccb9dfee805ae6edb0e7fef0989d0a0f3fc9f97bac423e068272a4b7b2303ecbfbcd33366d0f2f1b8bf00810dd748b58668c2e5'
+#DemoApp::Application.config.secret_key_base = #'61cbad0e755c925c75461c704ccb9dfee805ae6edb0e7fef0989d0a0f3fc9f97bac423e068272a4b7b2303ec#bfbcd33366d0f2f1b8bf00810dd748b58668c2e5'
+
+
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+SampleApp::Application.config.secret_key_base = secure_token
